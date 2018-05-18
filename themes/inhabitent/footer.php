07c4357ff7fa3cc1960a4db8d5908d8e13747b6c
aroerick <a.roerick@gmail.com>
2018-05-18 15:06:55 -0700

    Add basics of footer.

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,8 +11,28 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<div class="contact">
+						<h3 class="uppercase">contact info</h3>
+						<ul>
+							<li>info@inhabitent.com</li>
+							<li>778-456-7891</li>
+							<li> f t g+ i</li>
+						</ul>
+					</div>
+					<div class="hours">
+						<h3 class="uppercase">business hours</h3>
+						<ul>
+							<li><span class="bold">Monday-Friday:</span> 9am to 5pm</li>
+							<li><span class="bold">Saturday:</span> 10am to 2pm</li>
+							<li><span class="bold">Sunday:</span> Closed</li>
+					</div>
 				</div><!-- .site-info -->
+				<div class="footer-logo">
+					<img src="./assets/images/inhabitent-logo-text.svg" alt="Inhabitent Logo" />
+				</div>
+				<div class="legal">
+					<p class="uppercase">copyright &copy; 2018 inhabitent</p>
+				</div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
