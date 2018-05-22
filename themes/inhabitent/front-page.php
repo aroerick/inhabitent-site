d46a8766d9a3ce0db8d3d0fe25411cc2ff44efb4
aroerick <a.roerick@gmail.com>
2018-05-22 15:29:22 -0700

    Add main page header.

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -8,48 +8,24 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
+		
 		<main id="main" class="site-main" role="main">
+				<div class="overlay"></div>
+				<img class="site-logo" src="<?php echo get_template_directory_uri() ?>/assets/images/inhabitent-logo-full.svg" width="330px" alt="Main Inhabitent Logo" />
+				<?php if ( is_home() && ! is_front_page() ) : ?>
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+				<?php endif; ?>
 
-		<?php if ( have_posts() ) : ?>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+					<?php get_template_part( 'template-parts/content' ); ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php endwhile; ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
-                <?php if ( has_post_thumbnail() ) : ?>
-                    <?php the_post_thumbnail( 'large' ); ?>
-                <?php endif; ?>
-
-                <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-                <?php if ( 'post' === get_post_type() ) : ?>
-                <div class="entry-meta">
-                    <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-                </div><!-- .entry-meta -->
-                <?php endif; ?>
-            </header><!-- .entry-header -->
-
-            <div class="entry-content">
-                <?php the_excerpt(); ?>
-            </div><!-- .entry-content -->
-        </article><!-- #post-## -->
-
-		<?php endif; ?>
+				<?php the_posts_navigation(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
