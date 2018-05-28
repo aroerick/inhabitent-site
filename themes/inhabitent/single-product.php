8b44a2e024b53d3ed57173c99854dd5ccfdd5d24
aroerick <a.roerick@gmail.com>
2018-05-28 16:51:16 -0700

    Format product singles.

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -8,41 +8,23 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main container" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
-
-            <?php echo CFS()->get( 'price' ); ?>
 
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <header class="entry-header">
                 <?php if ( has_post_thumbnail() ) : ?>
                     <?php the_post_thumbnail( 'large' ); ?>
                 <?php endif; ?>
-
-                <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-                <div class="entry-meta">
-                    <?php inhabitent_posted_on(); ?> / <?php inhabitent_comment_count(); ?> / <?php inhabitent_posted_by(); ?>
-                </div><!-- .entry-meta -->
             </header><!-- .entry-header -->
 
             <div class="entry-content">
+                <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+                <h3 class="price"><?php echo CFS()->get( 'price' ); ?></h3>
                 <?php the_content(); ?>
-                <?php
-                    wp_link_pages( array(
-                        'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-                        'after'  => '</div>',
-                    ) );
-                ?>
             </div><!-- .entry-content -->
-
-            <footer class="entry-footer">
-                <?php inhabitent_entry_footer(); ?>
-            </footer><!-- .entry-footer -->
         </article><!-- #post-## -->
-            
-        <?php the_post_navigation(); ?>
 
 		<?php endwhile; // End of the loop. ?>
 
